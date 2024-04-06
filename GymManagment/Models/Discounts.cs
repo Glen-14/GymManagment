0879c7f5461b3604a84a8965249dfeaeb6dc2bd4
Perdorues <Perdorues@DESKTOP-CAS296I>
2024-04-06 11:17:55 +0200

    shtova discounts

--- GymManagment/Models/Discounts.cs
+++ GymManagment/Models/Discounts.cs
@@ -2,5 +2,11 @@
 {
     public class Discounts
     {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public decimal Value { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool DeactivationDate { get; set; }
     }
 }
