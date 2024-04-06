4233d3a7c9b96d99d4cf73e7a67057022c5b631c
Perdorues <Perdorues@DESKTOP-CAS296I>
2024-04-06 11:40:55 +0200

    shtova contextin

---
+++ GymManagment/Models/GymContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace GymManagment.Models
+{
+    public class GymContext: DbContext
+    {
+        public GymContext(DbContextOptions<GymContext> options) : base(options)
+        { }
+
+        public virtual DbSet<Members> Members { get; set; }
+        public virtual DbSet<Discounts> Dsicounts { get; set; }
+        public virtual DbSet<MemberSubscription> MemberSubscriptions { get; set; }
+        public virtual DbSet<DiscountedMemberSubscription> DiscountedMemberSubscriptions { get; set; }
+        public virtual DbSet<Subscription> Subscriptions { get; set; }
+    }
+}
