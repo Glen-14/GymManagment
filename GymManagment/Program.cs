7a9b56f72bb6dc849da581ae3f35b5bb7c1f4437
Marvi P <prekajmarvi@gmail.com>
2024-04-20 10:44:48 +0200

    revised some lines in Program.cs regarding CORS.

--- GymManagment/Program.cs
+++ GymManagment/Program.cs
@@ -1,11 +1,9 @@
 using GymManagment.Models;
 using GymManagment.Repository;
 using Microsoft.EntityFrameworkCore;
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddDbContext<GymContext>(options =>
                       options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
@@ -17,19 +15,27 @@ builder.Services.AddScoped<SubscriptionRepository>();
 builder.Services.AddScoped<MemberSubscripitonsRepository>();
 builder.Services.AddScoped<DiscountsRepository>();
 builder.Services.AddScoped<DiscountedMemberSubscriptionRepository>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*")
+                                            .AllowAnyMethod()
+                                            .AllowAnyHeader()
+                                            .SetIsOriginAllowedToAllowWildcardSubdomains(); ;
+                      });
+});
 var app = builder.Build();
-
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(MyAllowSpecificOrigins);
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
